3e55c987d4fa528645f5c9e11b6e7b39567a769a
Jason Petersen <jason@peterscene.com>
2019-12-12 19:49:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,9 +12,10 @@ define("COPYRIGHT_TEXT", "&copy; " . date("Y") . " " . COMPANY_NAME);
 define("ATTRIBUTION_TEXT", "Site design by Peterscene");
 define("ATTRIBUTION_URL", "https://peterscene.com");
 
+define("THISPAGE", $_SERVER["REQUEST_URI"]);
 define("THISPROTOCOL", stripos($_SERVER["SERVER_PROTOCOL"], "https") === 0 ? "https://" : "http://");
 define("THISDOMAIN", THISPROTOCOL . $_SERVER["HTTP_HOST"]);
-define("THISURL", THISDOMAIN . $_SERVER["REQUEST_URI"]);
+define("THISURL", THISDOMAIN . THISPAGE);
 define("ESCAPEDURL", htmlspecialchars(THISURL, ENT_QUOTES, 'UTF-8'));
 ?>
 <!DOCTYPE html>
